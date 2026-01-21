3c7eefc8ba5de7356db6e74e41b39b4a31f43d3e
Nauval Faiz Luthf Hisyam <nauvalfaizluthfhisyam33@gmail.com>
2026-01-21 10:12:43 +0700

    nauval_faiz

--- lib/widgets/icon_String/icon.dart
+++ lib/widgets/icon_String/icon.dart
@@ -9,11 +9,11 @@ final List <IconData> lightIcon = [
   Amicons.iconly_profile,
 ];
  final List <IconData> boldIcon = [
-   Amicons.iconly_home,
-   Amicons.iconly_chart,
-   Amicons.vuesax_notification,
-   Amicons.flaticon_signal_alt_sharp,
-   Amicons.iconly_profile,
+   Amicons.iconly_home_fill,
+   Amicons.iconly_chart_fill,
+   Amicons.vuesax_notification_fill,
+   Amicons.flaticon_signal_alt_sharp_fill,
+   Amicons.iconly_profile_fill,
  ];
  final List <String> navbel = [
    "Home",
